e6b24db62e6048cda9265015ed33d295bd158f97
Yan <yan.tacheck@dcx.ufpb.br>
2024-08-20 16:18:49 -0300

    commita pela amor

---
+++ src/main/java/br/ufpb/dcx/ayla/agenda/Contato.java
@@ -0,0 +1,9 @@
+package br.ufpb.dcx.ayla.agenda;
+
+public class Contato {
+    private String nome;
+    private int diaAniversario;
+    private int mesAniversario;
+
+    contato (String:nome, int:dia,int:mes  )
+}
