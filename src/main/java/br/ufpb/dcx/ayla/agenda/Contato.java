2c4a6246425fa2e95e16bebe507269d9364e3b3c
Yan <yan.tacheck@dcx.ufpb.br>
2024-08-20 16:39:19 -0300

    commita pela amor parte 2

--- src/main/java/br/ufpb/dcx/ayla/agenda/Contato.java
+++ src/main/java/br/ufpb/dcx/ayla/agenda/Contato.java
@@ -1,9 +1,62 @@
 package br.ufpb.dcx.ayla.agenda;
 
+import java.util.Objects;
+
 public class Contato {
     private String nome;
     private int diaAniversario;
     private int mesAniversario;
 
-    contato (String:nome, int:dia,int:mes  )
+    public Contato(String nome, int diaAniversario, int mesAniversario) {
+        this.nome = nome;
+        this.diaAniversario = diaAniversario;
+        this.mesAniversario = mesAniversario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getDiaAniversario() {
+        return diaAniversario;
+    }
+
+    public void setDiaAniversario(int diaAniversario) {
+        this.diaAniversario = diaAniversario;
+    }
+
+    public int getMesAniversario() {
+        return mesAniversario;
+    }
+
+    public void setMesAniversario(int mesAniversario) {
+        this.mesAniversario = mesAniversario;
+    }
+
+    @Override
+    public String toString() {
+        return "Contato{" +
+                "nome='" + nome + '\'' +
+                ", diaAniversario=" + diaAniversario +
+                ", mesAniversario=" + mesAniversario +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contato contato = (Contato) o;
+        return diaAniversario == contato.diaAniversario && mesAniversario == contato.mesAniversario && Objects.equals(nome, contato.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, diaAniversario, mesAniversario);
+    }
 }
+
